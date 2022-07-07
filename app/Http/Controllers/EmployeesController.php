156b1c461b2c8d69dedad16b64b9d5addb441930
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2022-07-07 15:18:04 +0300

    Employees are listed.

---
+++ app/Http/Controllers/EmployeesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Log;
+use Exception;
+use App\Models\Employee;
+
+class EmployeesController extends Controller
+{
+    // Get Employee List from database
+
+    public function getEmployeeList(){
+        try{
+            $employees = Employee::orderBy('id', 'DESC')->get();
+            return response()->json($employees);
+        }catch(Exception $e){
+            Log::error($e);
+        }
+    }
+}
---
+++ resources/js/components/employeeList/TableRow.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class TableRow extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <tr>
+                <th>{this.props.data.id}</th>
+                <td>{this.props.data.employee_name}</td>
+                <td>{this.props.data.salary}</td>
+                <td>@mdo</td>
+            </tr>
+        )
+    }
+}
+
+export default TableRow; 
